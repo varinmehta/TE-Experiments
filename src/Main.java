ab71a0430d671cd45b7213f5e1fcd7f5795d4d91
varinmehta <mehtavarin2004@gmail.com>
2024-10-02 09:59:12 +0530

    change 1

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,26 @@
-import java.util.*;
-public class Main{
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        for(int i = 0; i<n; i++){
-            int m = sc.nextInt();
-            sc.nextLine();
-            String s = sc.nextLine();
-            if(isPerfectSquare(m)){
-                int temp = (int)Math.sqrt(m);
-                boolean check = true;
-                for(int j = 0; j<(temp-1)*2; j++){
-                    if(s.charAt(j)!='1'){
-                        check = false;
-                        break;
-                    }
-                }
-                if(check) {
-                    for (int j = (temp - 1) * 2; j < m - (temp - 1) * 2; j++) {
-                        if (s.charAt(j) != '0') {
-                            check = false;
-                            break;
-                        }
-                    }
-                }
-                if(check) {
-                    for (int j = m - (temp - 1) * 2; j < m; j++) {
-                        if (s.charAt(j) != '1') {
-                            check = false;
-                            break;
-                        }
-                    }
-                }
-                if(check){
-                    if(m%2==0) System.out.println("YES");
-                }
-                if(!check) System.out.println("NO");
-                else System.out.println("YES");
-            } else {
-                System.out.println("NO");
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int[] arr = new int[n+1];
+            arr[0] = 0;
+            for (int j = 1; j <= n; j++) {
+                arr[j] = sc.nextInt();
             }
+            int max = 0;
+            for(int j = 0; j < n; j++){
+                int diff = arr[j+1] - arr[j];
+                max = Math.max(max, diff);
+            }
+            max = Math.max(max, (x-arr[n])*2);
+            System.out.println(max);
         }
         sc.close();
-    }
-    public static boolean isPerfectSquare(int x)
-    {
-        if (x >= 0) {
-            int sr = (int)Math.sqrt(x);
-            return (sr * sr == x);
-        }
-        return false;
     }
 }
